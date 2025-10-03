ae422610e417eaaab5bbc45411e65be9df723d2c
Jorge0827 <jorgean0827@gmail.com>
2025-10-03 18:23:38 -0500

    Crear proyecto inicial

---
+++ src/main/java/com/jechavarria/stationery_app/StationeryAppApplication.java
@@ -0,0 +1,13 @@
+package com.jechavarria.stationery_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationeryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationeryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jechavarria/stationery_app/StationeryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jechavarria.stationery_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationeryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
